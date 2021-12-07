8c07b8ad51feaa8cbba71a0a7625f3cfb9a51f72
YangLock <3167706386@qq.com>
2021-12-07 16:57:21 +0800

    Add: Loss & Metrics

--- NMT_en2zh/train.py
+++ NMT_en2zh/train.py
@@ -34,8 +34,25 @@ class CustomSchedule(tfkeras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 learning_rate = CustomSchedule(d_model)
-optimizer = tfkeras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+optimizer = tfkeras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-8)
+
+# ---------------- Loss & Metrics ---------------- #
+loss_object = tfkeras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))    # 由于target序列也都是padding过的，所以要将填充为0的那部分筛选出来，填充值部分是不参与损失值计算的
+    loss_ = loss_object(real, pred)
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+    return tf.reduce_mean(loss_)
+
+train_loss = tfkeras.metrics.Mean(name='train_loss')
+train_accuracy = tfkeras.metrics.SparseCategoricalCrossentropy(name='train_accuracy')
 
 if __name__ == '__main__':
-    print(train_data)
-    print(tokenizer_en.vocab_size, tokenizer_cn.vocab_size)
+    real = [9, 12, 0, 31, 0, 0, .31, 0, 0, 0, 19, 15, 1, 1]
+    flags = tf.math.equal(real, 0)
+    mask = tf.math.logical_not(flags)
+    mask = tf.cast(mask, dtype=tf.float32)
+    print(flags)
+    print(mask)
